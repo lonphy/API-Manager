e3b68c1a1d1ac5e58e99f355dbd1d5bf0b9bf989
lonphy <admin@lonphy.com>
2014-07-04 23:23:54 +0800

    走通整个流程

---
+++ js/conf.js
@@ -0,0 +1,14 @@
+/**
+ * APP全局配置信息
+ * @type {{db: {name: string, version: number}, service: {path: string}, dao: {path: string}}}
+ */
+var L5_CONF = {
+    db: {
+        name:"l5-api",
+        version:1
+    },
+
+    module: {
+        path: "module/"
+    }
+};
